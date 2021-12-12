b828be3cf57883c81388af98b2457a0d8450affa
Maycon-Natan <mayconnatan7@gmail.com>
2021-12-12 17:24:58 -0300

    barra de pesquisa

--- lib/controllers/movie_controller.dart
+++ lib/controllers/movie_controller.dart
@@ -10,8 +10,17 @@ class MovieController {
   }
 
   ValueNotifier<Movies?> movies = ValueNotifier<Movies?>(null);
+  Movies? _cachedMovies;
+
+  onChanged(String value) {
+    List<Results> list = _cachedMovies!.results
+        .where((e) => e.toString().toLowerCase().contains(value.toLowerCase()))
+        .toList();
+    movies.value = movies.value!.copyWith(results: list);
+  }
 
   fetch() async {
     movies.value = await _moviesRepository.getMovies();
+    _cachedMovies = movies.value;
   }
 }
